5b3981e8e419ec21b39041c15d574e4e91950515
donnaif <56908489+donnaif@users.noreply.github.com>
2023-04-14 00:29:45 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,28 @@
-const generateBtn = document.getElementById('generate-btn');
-const nameInput = document.getElementById('name');
-const greetingMessage = document.getElementById('greeting');
+function createImage() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  const img = new Image();
+  img.onload = function() {
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+  };
+  img.src = 'https://up6.cc/2023/04/168142117409381.png/400/200';
+}
 
-generateBtn.addEventListener('click', function() {
-  const name = nameInput.value;
-  greetingMessage.innerText = `Happy Birthday, ${name}!`;
-  
-  html2canvas(document.querySelector('.card')).then(function(canvas) {
-    // Convert canvas to image and save it
-    const image = canvas.toDataURL('/images/image.png');
-    const link = document.createElement('a');
-    link.download = 'greeting-card.png';
-    link.href = image;
-    link.click();
-  });
+const clearBtn = document.getElementById('clear-btn');
+clearBtn.addEventListener('click', function() {
+  const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+});
+
+const saveBtn = document.getElementById('save-btn');
+saveBtn.addEventListener('click', function() {
+  const canvas = document.getElementById('canvas');
+  const dataURL = canvas.toDataURL('image/png');
+  const link = document.createElement('a');
+  link.download = 'canvas-image.png';
+  link.href = dataURL;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
 });
